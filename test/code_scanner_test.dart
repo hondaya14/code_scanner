a13b55749905c9829c27dd9d0dc3576650a79ad9
Eduardo dos santos <megamil@me.com>
2023-01-16 20:28:44 -0300

    Update flutter version to android

--- test/code_scanner_test.dart
+++ test/code_scanner_test.dart
@@ -1,18 +1,1 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  const MethodChannel channel = MethodChannel('code_scanner');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-}
+void main() {}
